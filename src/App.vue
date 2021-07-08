e029b37e10f33ae3e2ecafa5a4c24782dbd81694
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-07-08 01:04:01 -0300

    chore: import new components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
 
-  <FirstComponent />
-  <LifeCycle />
+<div>
+    <FirstComponent />
+    <LifeCycle />
+    <Person />
+</div>
 
 </template>
 
 <script>
   import FirstComponent from './components/FirstComponent.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import Person from './components/Person.vue'
 
   export default {
     name: 'App',
     components: {
       FirstComponent,
-      LifeCycle
+      LifeCycle,
+      Person
     }
   }
 </script>
