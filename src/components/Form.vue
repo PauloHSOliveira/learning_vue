c63469933c60459af448af6bd4fd0a51800cebd5
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-07-08 01:03:20 -0300

    feat: create form

---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form>
+            <div>
+                 <InputText />
+            </div>
+            <div>
+                 <InputText />
+            </div>
+            <div>
+                <Submit />
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import InputText from './form/InputText.vue'
+import Submit from './form/Submit.vue'
+
+export default { 
+    name:'Form',
+    components: {
+        InputText,
+        Submit
+    }
+}
+</script>
