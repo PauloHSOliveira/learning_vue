2a398e7724258f5e63e15165854e969bb6b0e7cb
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-07-08 01:03:50 -0300

    feat: create person

---
+++ src/components/Person.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Me chamo {{name}}</h2>
+        <Info />
+        <Form />
+    </div>
+</template>
+
+<script>
+
+import Info from './Info.vue'
+import Form from './Form.vue'
+
+export default {
+    name:'Person',
+    components: {
+        Info,
+        Form
+    },
+    data() {
+        return {
+            name:'Paulo'
+        }
+    }
+}
+</script>
