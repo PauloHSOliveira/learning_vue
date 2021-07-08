d3bbd13cac4e34b0ee2b83b0fcd47ebf70242241
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-07-08 01:03:35 -0300

    feat: create info component

---
+++ src/components/Info.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>React</li>
+            <li>Node</li>
+            <li>Vue</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Info'
+}
+</script>
